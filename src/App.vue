0bfbb90c22af97cab91882afe904b7a7306ad2af
Louis Smith <15144861+smudger@users.noreply.github.com>
2023-07-17 16:11:25 +0100

    Add yjs with local WebRTC

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,31 @@
-<script setup></script>
+<script setup>
+import * as Y from "yjs";
+import { WebrtcProvider } from "y-webrtc";
+import { onMounted, ref } from "vue";
+
+// A new Y document
+const ydoc = new Y.Doc();
+// Registered with a WebRTC provider
+const provider = new WebrtcProvider(
+  "ce004c03-9927-40e7-9eba-3fe63f73d84e",
+  ydoc,
+  {
+    signaling: ["ws://localhost:4444"],
+  },
+);
+
+const yarray = ydoc.get("array", Y.Array);
+
+const myArray = ref(yarray.toArray());
+
+yarray.observe(() => {
+  myArray.value = yarray.toArray();
+});
+
+onMounted(() => yarray.insert(0, ["New User"]));
+</script>
 
 <template>
-  <h1 class="font-mono bg-green-100">Hi there!</h1>
+  <h1>Hi there!</h1>
+  {{ myArray }}
 </template>
