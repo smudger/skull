1ebf0f5b034cbf8fe58d91b1cdd10aebb428a24c
Louis Smith <15144861+smudger@users.noreply.github.com>
2023-07-19 10:32:30 +0100

    Change casing of router link

--- src/Pages/TheNotFound.vue
+++ src/Pages/TheNotFound.vue
@@ -13,10 +13,10 @@
         Sorry, we couldn’t find the page you’re looking for.
       </p>
       <div class="mt-10 flex items-center justify-center gap-x-6">
-        <router-link
+        <RouterLink
           :to="{ name: 'Home' }"
           class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-          >Go back home</router-link
+          >Go back home</RouterLink
         >
       </div>
     </div>
