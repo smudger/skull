4123e1e353f4ae0152b1667140f746f5671912db
Louis Smith <15144861+smudger@users.noreply.github.com>
2023-09-04 17:52:20 +0100

    Update styling of home page and not found page

--- src/Pages/TheHome.vue
+++ src/Pages/TheHome.vue
@@ -7,14 +7,14 @@ const code = Array(6)
 </script>
 
 <template>
-  <div class="bg-white">
+  <div class="bg-gray-900 min-h-screen">
     <div class="relative isolate px-6 pt-14 lg:px-8">
       <div
         aria-hidden="true"
         class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
       >
         <div
-          class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+          class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#fef3c7] to-[#f59e0b] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
           style="
             clip-path: polygon(
               74.1% 44.1%,
@@ -40,13 +40,13 @@ const code = Array(6)
       <div class="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
         <div class="text-center">
           <h1
-            class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl"
+            class="text-4xl font-bold tracking-tight text-gray-100 sm:text-6xl"
           >
-            Planning poker kept simple
+            Let's play Skull
           </h1>
-          <p class="mt-6 text-lg leading-8 text-gray-600">
-            No fees, no registration, no ads. Just a simple, free, online
-            planning poker app when you need it.
+          <p class="mt-6 text-lg leading-8 text-gray-400">
+            No fees, no registration, no ads. Just a free, online implementation
+            of the card game Skull.
           </p>
           <div class="mt-10 flex items-center justify-center gap-x-6">
             <RouterLink
@@ -55,7 +55,7 @@ const code = Array(6)
                 params: { id },
                 query: { code },
               }"
-              class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              class="rounded-md bg-amber-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-amber-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-amber-600"
               >Start a new game</RouterLink
             >
           </div>
@@ -63,10 +63,10 @@ const code = Array(6)
       </div>
       <div
         aria-hidden="true"
-        class="absolute inset-x-0 top-[calc(100%-17rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-33rem)]"
+        class="absolute inset-x-0 top-[calc(100%-17rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-37rem)]"
       >
         <div
-          class="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+          class="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr to-[#fef3c7] from-[#f59e0b] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
           style="
             clip-path: polygon(
               74.1% 44.1%,
